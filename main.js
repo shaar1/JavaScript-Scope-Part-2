a07da69482b8eaeaff3724ed2d7a9a37dab35072
Abdallah <abdallahchaar90@gmail.comh>
2021-01-08 15:57:46 +0100

    träning om function

---
+++ main.js
@@ -0,0 +1,41 @@
+/* golbal console, alert, prompt*/
+
+/*
+var x = 1; 
+
+function testFunc(){
+
+    "use strict";
+
+    var calc = x + 2;
+    console.log(calc)
+}
+testFunc();
+
+*/
+/*
+//var x = 1; 
+
+function testFunc(){
+
+    "use strict";
+
+    var calc = x + 2; // 3
+    console.log(calc)
+}
+testFunc();
+*/
+
+var x = 1;
+
+function testFunc() {
+
+    "use strict";
+
+    var x = 5,
+
+        calc = x + 2;
+        
+    console.log(calc)
+}
+testFunc();
