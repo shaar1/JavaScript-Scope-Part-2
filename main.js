3631f842cce2ae26ab1d2867672a160c6c3df2be
Abdallah <abdallahchaar90@gmail.comh>
2021-01-08 16:19:10 +0100

    träning om javaScrit med scope och return

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function testFunc(){
 }
 testFunc();
 */
-
+/*
 var x = 1;
 
 function testFunc() {
@@ -35,7 +35,100 @@ function testFunc() {
     var x = 5,
 
         calc = x + 2;
-        
+
     console.log(calc)
 }
 testFunc();
+*/
+/*
+var x = 1;
+
+function testFunc() {
+
+    "use strict";
+
+    var x = 5,
+
+        calc = x + 2;
+
+    console.log(calc)
+}
+testFunc();
+
+console.log(x + 2);
+*/
+/*
+var x = 1;
+
+function testFunc() {
+
+    "use strict";
+
+    var x = 5,
+
+        calc = x + 2; // 5 + 2 = 7
+
+    console.log(calc)
+}
+testFunc();
+
+console.log(x + 2);// 1 + 2 = 3
+*/
+
+/*
+var x = 1;
+
+function testFunc() {
+
+    "use strict";
+
+    x = 5;
+
+        var calc = x + 2;
+
+    console.log(calc)
+}
+testFunc();
+
+console.log(x + 2);
+*/
+/*
+var x = 1;
+
+function testFunc() {
+
+    "use strict";
+
+    var x = 5;
+
+    function testChild() {
+
+        var calc = x + 2;
+    }
+    console.log(calc)
+    }
+    testFunc();
+
+    console.log(x + 2);
+    */
+
+   var x = 1;
+
+   function testFunc() {
+   
+       "use strict";
+   
+       var x = 5;
+   
+       function testChild() {
+   
+           var calc = x + 2;
+
+           console.log(calc)
+
+       }
+       return testChild();
+       }
+       testFunc();
+   
+       console.log(x + 2);
