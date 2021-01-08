72f3624725bf069b611864a4c50abe1b281639ea
Abdallah <abdallahchaar90@gmail.comh>
2021-01-08 16:41:13 +0100

    träning om olika plats för console och med vem räknar

--- main.js
+++ main.js
@@ -180,7 +180,7 @@ function testParent() {
 
     console.log(x + 2);
     */
-
+/*
 var x = 1;
 
 function testParent() {
@@ -203,4 +203,104 @@ function testParent() {
 
 testParent();
 
+console.log(x + 2);
+*/
+/*
+var x = 1;
+
+// scope A
+// x Is Accessed From Everywhere
+
+function testParent() {
+
+    // Scope B
+    // x Is Accessed From Scope B
+
+
+    "use strict";
+
+    var x = 5;
+
+    function testChild() {
+
+        // scope C
+
+            // x Is Accessed From Scope c
+
+        var x = 100,
+
+            calc = x + 2;
+
+        console.log(calc);
+
+    }
+
+    return testChild();
+}
+
+testParent();
+
+console.log(x + 2);
+*/
+
+
+/*
+var x = 1;
+
+// scope A
+// x Is Accessed From Everywhere
+
+function testParent() {
+
+    // Scope B
+    // x Is Accessed From Scope B
+
+
+    "use strict";
+
+    var x = 5;
+
+    function testChild() {
+
+        // scope C
+
+        // x Is Accessed From Scope c
+
+        var x = 100,
+
+            calc = x + 2;
+
+        console.log(calc);
+
+    }
+    console.log(x);
+
+    return testChild();
+}
+
+testParent();
+
+console.log(x + 2);
+
+*/
+var x = 1;
+// scope A
+// x Is Accessed From Everywhere
+function testParent() {
+
+    // Scope B
+    "use strict";
+
+    var x = 5;
+    function testChild() {
+        // scope C
+
+        var calc = x + 2;
+        console.log(calc);
+        console.log(x);
+    }
+    console.log(x);
+    return testChild();
+}
+testParent();
 console.log(x + 2);
